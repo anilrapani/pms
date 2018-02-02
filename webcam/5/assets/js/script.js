850bb89f4e24a36aaee00c1a4b3ed808d32fdb70
anilrapani <anil.rapani@gmail.com>
2018-02-02 23:45:14 +0530

    cam

---
+++ webcam/5/assets/js/script.js
@@ -0,0 +1,212 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    // References to all the element we will need.
+    var video = document.querySelector('#camerastream'),
+        image = document.querySelector('#snap'),
+        start_camera = document.querySelector('#start-camera'),
+        controls = document.querySelector('.controls'),
+        take_photo_btn = document.querySelector('#take-photo'),
+        delete_photo_btn = document.querySelector('#delete-photo'),
+        download_photo_btn = document.querySelector('#download-photo'),
+        error_message = document.querySelector('#error-message');
+
+
+    // The getUserMedia interface is used for handling camera input.
+    // Some browsers need a prefix so here we're covering all the options
+    /*navigator.getMedia = ( navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia);
+*/
+//navigator.mediaDevices
+//      .getUserMedia({
+//        // audio: true,
+//        video: {
+//          facingMode: {exact: "environment"}
+//        }
+//      })
+//      .then(function(stream) {
+//        video.srcObject = stream;
+//      });
+      
+      
+        
+
+         
+
+         // var log = msg => div.innerHTML += msg + "<br>";
+          
+          
+          
+ navigator.getMedia = navigator.getUserMedia;
+    if(!navigator.getUserMedia){
+        displayErrorMessage("Your browser doesn't have support for the navigator.getUserMedia interface.");
+    }
+    else{
+
+        // Request the camera.
+        navigator.getUserMedia(
+            {
+                video: true
+            },
+            // Success Callback
+            function(stream){
+     var getUserMedia = c => {
+            var face = c.camerastream && c.camerastream.facingMode;
+            face = face && ((typeof face === 'object') ? face : {ideal: face});
+
+            if (!(face && (face.exact == "environment" || face.ideal == "environment")) ||
+                (navigator.mediaDevices.getSupportedConstraints &&
+                 navigator.mediaDevices.getSupportedConstraints().facingMode)) {
+              return navigator.mediaDevices.getUserMedia(c);
+            }
+            // Polyfill "environment" facingMode. Look for "back" in label, or last cam (usually back).
+            delete c.camerastream.facingMode;
+            return navigator.mediaDevices.enumerateDevices()
+            .then(devices => {
+              devices = devices.filter(d => d.kind == "videoinput");
+              var back = devices.find(d => d.label.toLowerCase().indexOf("back") != -1) ||
+                         (devices.length && devices[devices.length-1]);
+              if (back) {
+                c.camerastream.deviceId = face.exact? { exact: back.deviceId } : { ideal: back.deviceId };
+              }
+              return navigator.mediaDevices.getUserMedia(c);
+            });
+          };
+                // Create an object URL for the video stream and
+                // set it as src of our HTLM video element.
+          //      video.srcObject = stream
+ getUserMedia({ video: { facingMode: "environment" } })
+            .then(stream => camerastream.srcObject = stream)
+            .then(camerastream.play())
+    .then(camerastream.onplay = function() {
+                    showVideo();
+                })
+            .catch(e => log(e.name + ": "+ e.message));
+                // Play the video element to start the stream.
+                
+                ;
+         
+            },
+            // Error Callback
+            function(err){
+                displayErrorMessage("There was an error with accessing the camera stream: " + err.name, err);
+            }
+        );
+
+    }
+
+
+
+    // Mobile browsers cannot play video without user input,
+    // so here we're using a button to start it manually.
+    start_camera.addEventListener("click", function(e){
+
+        e.preventDefault();
+
+        // Start video playback manually.
+        video.play();
+        showVideo();
+
+    });
+
+
+    take_photo_btn.addEventListener("click", function(e){
+
+        e.preventDefault();
+
+        var snap = takeSnapshot();
+
+        // Show image. 
+        image.setAttribute('src', snap);
+        image.classList.add("visible");
+
+        // Enable delete and save buttons
+        delete_photo_btn.classList.remove("disabled");
+        download_photo_btn.classList.remove("disabled");
+
+        // Set the href attribute of the download button to the snap url.
+        download_photo_btn.href = snap;
+
+        // Pause video playback of stream.
+        video.pause();
+
+    });
+
+
+    delete_photo_btn.addEventListener("click", function(e){
+
+        e.preventDefault();
+
+        // Hide image.
+        image.setAttribute('src', "");
+        image.classList.remove("visible");
+
+        // Disable delete and save buttons
+        delete_photo_btn.classList.add("disabled");
+        download_photo_btn.classList.add("disabled");
+
+        // Resume playback of stream.
+        video.play();
+
+    });
+
+
+  
+    function showVideo(){
+        // Display the video stream and the controls.
+
+        hideUI();
+        video.classList.add("visible");
+        controls.classList.add("visible");
+    }
+
+
+    function takeSnapshot(){
+        // Here we're using a trick that involves a hidden canvas element.  
+
+        var hidden_canvas = document.querySelector('canvas'),
+            context = hidden_canvas.getContext('2d');
+
+        var width = video.videoWidth,
+            height = video.videoHeight;
+
+        if (width && height) {
+
+            // Setup a canvas with the same dimensions as the video.
+            hidden_canvas.width = width;
+            hidden_canvas.height = height;
+
+            // Make a copy of the current frame in the video on the canvas.
+            context.drawImage(video, 0, 0, width, height);
+
+            // Turn the canvas image into a dataURL that can be used as a src for our photo.
+            return hidden_canvas.toDataURL('image/png');
+        }
+    }
+
+
+    function displayErrorMessage(error_msg, error){
+        error = error || "";
+        if(error){
+            console.error(error);
+        }
+
+        error_message.innerText = error_msg;
+
+        hideUI();
+        error_message.classList.add("visible");
+    }
+
+   
+    function hideUI(){
+        // Helper function for clearing the app UI.
+
+        controls.classList.remove("visible");
+        start_camera.classList.remove("visible");
+        video.classList.remove("visible");
+        snap.classList.remove("visible");
+        error_message.classList.remove("visible");
+    }
+
+});
